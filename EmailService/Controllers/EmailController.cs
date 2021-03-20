1d22ebd2837148600d8188aa2f222bf89f872de3
klein1992 <ejw695@gmail.com>
2021-03-20 17:37:25 -0400

    Initial commit

---
+++ EmailService/Controllers/EmailController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace EmailService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmailController : ControllerBase
+    {
+        private readonly EmailInterface emailService;
+        public EmailController(EmailInterface emailService) => this.emailService = emailService;
+
+        [HttpPost]
+        public async Task<IActionResult> SendEmail(OrderDetailsDto order)
+        {
+            await emailService.SendEmail(order);
+            return Ok();
+        }
+
+
+    }
+}
---
+++ EmailService/OrderDetailsDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmailService
+{
+    public class OrderDetailsDto
+    {
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+        public string email { get; set; }
+
+        public DateTime pickupTime { get; set; }
+    }
+}
